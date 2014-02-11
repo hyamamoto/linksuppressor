4df1dd772763d37a5a83bafd38ebcb52dd5c49cb
Hiroshi Yamamoto <hyamamoto2000@gmail.com>
2014-02-11 18:34:16 +0900

    initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-linksuppressor
+LinkSuppressor
 ==============
+	
 
-LinkSuppressor is a lightweight Chrome extension that enables the intuitive link text selection.
+<code>LinkSuppressor</code> is a lightweight extension that enables an intuitive partial link selection by suppressing Chrome's "Drag &amp; Drop Link" feature. All you have to do is selecting a link with any of [Alt] / [Window] / [Command] key being pressed. Since this extension temporarily suppresses a single unwanted feature, any of the meta information associated with the original text won't be lost when you copy-and-paste into other rich-text editors.
+
+Usage:
+
+* Windows : Press <code>[Alt]</code> or <code>[Windows]</code> while selecting a link
+* Mac OS X: Press <code>[Alt]</code> or <code>[Command]</code> while selecting a link
+* Other   : Press <code>[Alt]</code> or <code>[Meta]</code> while selecting a link
+
+
+Installation
+------------
+
+### From Chrome Web Store
+
+It's on Chrome WebStore. [Download from here](https://chrome.google.com/webstore/detail/jffdmjmhkinopagcjgfjjfoagglhnhff/).
+
+### From Source Code
+
+* Clone the repo.
+* In Chrome, choose Window > Extensions.  
+Enter the Develop Mode and add the linksupressor-chrome directory through 'Add Unpacked Extension'.
+
---
+++ linksuppressor-chrome/src/link_suppressor.js
@@ -0,0 +1,68 @@
+﻿// "Link Suppressor: Make any Link Selectable" extension
+// Hiroshi Yamamoto (higon@freepress.jp)
+
+var checkInvokeMetaKey;
+if (navigator.appVersion.indexOf("Mac")!=-1) {
+    checkInvokeMetaKey = function(e) { return e.metaKey == true || e.altKey == true;};
+} else{
+	checkInvokeMetaKey = function(e) {
+	  return e.metaKey == true || (e.ctrlKey == true && e.shiftKey == true);};
+}
+
+function checkInvokable( e) {
+  var a = huntAnchor(e.target);
+  return checkInvokeMetaKey(e) && a && a.tagName.toUpperCase() === 'A';
+}
+
+function huntAnchor( elem) {
+  return (elem.tagName && elem.tagName.toUpperCase() === 'A') ? elem:
+   (elem.parentNode ? huntAnchor(elem.parentNode): null);
+}
+
+var fastClickDetection = false;
+
+function onMouseDown(e) {
+	if (checkInvokable(e)){
+	    var element = huntAnchor(e.target);
+		var url = element.href;
+		// console.log("[LinkSuppressor]: a link has been exterminated");
+		element.removeAttribute("href"); // Kill a link
+		fastClickDetection = true;
+		setTimeout( function() {
+		    fastClickDetection = false;
+		}, 150);
+		summonReanimator(element, url);
+		return false;
+	}
+}
+
+function summonReanimator(element, url) {
+    var reanimator = function(e) {
+        console.log("[LinkSuppressor]: a link has been resurrected");
+		if (element) {
+		    window.removeEventListener("click", arguments.callee);
+		    if (url) {
+		        var resurrect = function(){
+		            element.setAttribute("href",url);
+		        };
+		        if ( fastClickDetection) {
+		            resurrect();
+		        } else {
+		            setTimeout( resurrect, 100);
+		        }
+		    }
+		    if (e.type ==="mouseup") {
+		        window.removeEventListener("mouseup", arguments.callee);
+		    }
+		}
+		e.stopPropagation();
+		e.preventDefault();
+	    return false;
+	};
+	window.addEventListener("click", reanimator);
+	window.addEventListener("mouseup", reanimator);
+}
+
+window.addEventListener('mousedown', onMouseDown);
+
+
