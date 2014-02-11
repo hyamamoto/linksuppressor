c99f43a8ba5fae37fa6b028697e3034ec47078df
Hiroshi Yamamoto <hyamamoto2000@gmail.com>
2014-02-11 23:10:34 +0900

    versino 1.0.3: fixes for windows. added a 'news.google.com' hack.

--- README.md
+++ README.md
@@ -2,13 +2,7 @@ LinkSuppressor
 ==============
 	
 
-<code>LinkSuppressor</code> is a lightweight extension that enables an intuitive partial link selection by suppressing Chrome's "Drag &amp; Drop Link" feature. All you have to do is selecting a link with any of [Alt] / [Window] / [Command] key being pressed. Since this extension temporarily suppresses a single unwanted feature, any of the meta information associated with the original text won't be lost when you copy-and-paste into other rich-text editors.
-
-Usage:
-
-* Windows : Press <code>[Alt]</code> or <code>[Windows]</code> while selecting a link
-* Mac OS X: Press <code>[Alt]</code> or <code>[Command]</code> while selecting a link
-* Other   : Press <code>[Alt]</code> or <code>[Meta]</code> while selecting a link
+<code>LinkSuppressor</code> is a lightweight extension that enables an intuitive partial link selection by suppressing Chrome's "Drag &amp; Drop Link" feature. All you have to do is selecting a link with any of <code>[Alt]</code> / <code>[Meta]</code> / <code>[Command]</code> key being pressed. Since this extension temporarily suppresses a single unwanted feature, any of the meta information associated with the original text won't be lost when you copy-and-paste into other rich-text editors.
 
 
 Installation
@@ -16,7 +10,7 @@ Installation
 
 ### From Chrome Web Store
 
-It's on Chrome WebStore. [Download from here](https://chrome.google.com/webstore/detail/jffdmjmhkinopagcjgfjjfoagglhnhff/).
+It's on Chrome Web Store already. [Download from here](https://chrome.google.com/webstore/detail/jffdmjmhkinopagcjgfjjfoagglhnhff/).
 
 ### From Source Code
 
@@ -24,3 +18,10 @@ It's on Chrome WebStore. [Download from here](https://chrome.google.com/webstore
 * In Chrome, choose Window > Extensions.  
 Enter the Develop Mode and add the linksupressor-chrome directory through 'Add Unpacked Extension'.
 
+
+Usage
+-----
+
+* Windows : Press <code>[Alt]</code> or <code>[Ctrl+Shift]</code> while selecting a link
+* Mac OS X: Press <code>[Alt]</code> or <code>[Command]</code> while selecting a link
+* Other   : Press <code>[Alt]</code> or <code>[Meta]</code> while selecting a link
--- linksuppressor-chrome/src/link_suppressor.js
+++ linksuppressor-chrome/src/link_suppressor.js
@@ -1,68 +1,86 @@
-﻿// "Link Suppressor: Make any Link Selectable" extension
-// Hiroshi Yamamoto (higon@freepress.jp)
+﻿// "Link Suppressor: Make any Link Selectable" Chrome extension
+// @version 1.0.2
+// @author Hiroshi Yamamoto (higon@freepress.jp)
+// @license MIT License 
 
 var checkInvokeMetaKey;
-if (navigator.appVersion.indexOf("Mac")!=-1) {
-    checkInvokeMetaKey = function(e) { return e.metaKey == true || e.altKey == true;};
-} else{
-	checkInvokeMetaKey = function(e) {
-	  return e.metaKey == true || (e.ctrlKey == true && e.shiftKey == true);};
+if (navigator.appVersion.indexOf("Mac") != -1) {
+    checkInvokeMetaKey = function (e) {
+        return e.metaKey == true || e.altKey == true;
+    };
+} else {
+    checkInvokeMetaKey = function (e) {
+        return e.metaKey == true || (e.ctrlKey == true && e.shiftKey == true) || e.altKey == true;
+    };
 }
 
-function checkInvokable( e) {
-  var a = huntAnchor(e.target);
-  return checkInvokeMetaKey(e) && a && a.tagName.toUpperCase() === 'A';
+function checkInvokable(e) {
+    var a = huntAnchor(e.target);
+    return checkInvokeMetaKey(e) && a;
 }
 
-function huntAnchor( elem) {
-  return (elem.tagName && elem.tagName.toUpperCase() === 'A') ? elem:
-   (elem.parentNode ? huntAnchor(elem.parentNode): null);
+function huntAnchor(elem) {
+    return (elem.tagName && elem.tagName.toUpperCase() === 'A') ? elem :
+        (elem.parentNode ? huntAnchor(elem.parentNode) : null);
 }
 
 var fastClickDetection = false;
 
 function onMouseDown(e) {
-	if (checkInvokable(e)){
-	    var element = huntAnchor(e.target);
-		var url = element.href;
-		// console.log("[LinkSuppressor]: a link has been exterminated");
-		element.removeAttribute("href"); // Kill a link
-		fastClickDetection = true;
-		setTimeout( function() {
-		    fastClickDetection = false;
-		}, 150);
-		summonReanimator(element, url);
-		return false;
-	}
+    var element2 = huntAnchor(e.target);
+    if (checkInvokable(e)) {
+        var element = huntAnchor(e.target);
+        var url = element.href;
+        // console.log("[LinkSuppressor]: a link has been exterminated");
+        element.removeAttribute("href"); // Kill a link
+        fastClickDetection = true;
+        setTimeout(function () {
+            fastClickDetection = false;
+        }, 150);
+        summonReanimator(element, url);
+        return false;
+    }
 }
 
 function summonReanimator(element, url) {
-    var reanimator = function(e) {
-        console.log("[LinkSuppressor]: a link has been resurrected");
-		if (element) {
-		    window.removeEventListener("click", arguments.callee);
-		    if (url) {
-		        var resurrect = function(){
-		            element.setAttribute("href",url);
-		        };
-		        if ( fastClickDetection) {
-		            resurrect();
-		        } else {
-		            setTimeout( resurrect, 100);
-		        }
-		    }
-		    if (e.type ==="mouseup") {
-		        window.removeEventListener("mouseup", arguments.callee);
-		    }
-		}
-		e.stopPropagation();
-		e.preventDefault();
-	    return false;
-	};
-	window.addEventListener("click", reanimator);
-	window.addEventListener("mouseup", reanimator);
+    var reanimator = function (e) {
+        // console.log("[LinkSuppressor]: a link has been resurrected");
+        if (element) {
+            window.removeEventListener("click", arguments.callee);
+            if (url) {
+                var resurrect = function () {
+                    element.setAttribute("href", url);
+                };
+                if (fastClickDetection) {
+                    resurrect();
+                } else {
+                    setTimeout(resurrect, 100);
+                }
+            }
+            if (e.type === "mouseup") {
+                window.removeEventListener("mouseup", arguments.callee);
+            }
+        }
+        e.stopPropagation();
+        e.preventDefault();
+        return false;
+    };
+    window.addEventListener("click", reanimator, false);
+    window.addEventListener("mouseup", reanimator, false);
 }
 
-window.addEventListener('mousedown', onMouseDown);
-
-
+var mousedownAdded = false;
+window.addEventListener('load',function(e){
+    if ( !mousedownAdded ) {
+        mousedownAdded = true;
+        window.addEventListener('mousedown', onMouseDown, false);
+    }
+    var currentUrl = window.location.href;
+    if ( currentUrl && currentUrl.indexOf("://news.google.com") != -1) {
+        var elements = document.getElementsByClassName("article");
+        for (i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            element.addEventListener('mousedown', onMouseDown, false);
+        }
+    }
+}, false);
